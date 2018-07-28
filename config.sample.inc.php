7c956b48af2c1ba457a2a75413edacc820a7678c
mnatarajmca <39547326+mnatarajmca@users.noreply.github.com>
2018-07-28 19:21:01 +0530

    Update config.sample.inc.php

--- config.sample.inc.php
+++ config.sample.inc.php
@@ -29,7 +29,7 @@ $i++;
 /* Authentication type */
 $cfg['Servers'][$i]['auth_type'] = 'cookie';
 /* Server parameters */
-$cfg['Servers'][$i]['host'] = 'mysql';
+$cfg['Servers'][$i]['host'] = '172.30.14.209';
 $cfg['Servers'][$i]['compress'] = false;
 $cfg['Servers'][$i]['AllowNoPassword'] = false;
 
